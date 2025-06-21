7ebba77877588149fb2f96ee50e6ce60010529ba
Santiago Sielecki <santi.siele@gmail.com>
2025-06-21 21:04:19 +0000

    mejora de estructura

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,6 @@
-import { guardarAlumno, buscarAlumno } from './services/AlumnoService';
+import * as dotenv from "dotenv";
+import { iniciarBot } from "./bot";
 
-// Prueba para guardar un alumno
-async function main() {
-  await guardarAlumno({
-    nombre: "Juan Pérez",
-    comentarios: "Lesión de rodilla",
-    rutinas: [
-      {
-        dia: "Lunes",
-        ejercicios: [
-          { nombre: "Prensa", series: "4x10", peso: "80kg" },
-          { nombre: "Bicicleta", tiempo: "15min" }
-        ]
-      },
-      {
-        dia: "Miércoles",
-        ejercicios: [
-          { nombre: "Curl de bíceps", series: "3x12", peso: "20kg" }
-        ]
-      }
-    ]
-  });
+dotenv.config();
 
-  const alumno = await buscarAlumno("Juan Pérez");
-  console.log("Resultado de la búsqueda:", alumno);
-}
-
-main();
+iniciarBot();
