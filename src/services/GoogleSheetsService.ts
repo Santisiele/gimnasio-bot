dcb86481f0cad95f763d2bf2c3de6f2a59042c5d
Santiago Sielecki <santi.siele@gmail.com>
2025-07-07 17:52:06 +0000

    mejora de memoria

--- src/services/GoogleSheetsService.ts
+++ src/services/GoogleSheetsService.ts
@@ -1,34 +1,33 @@
 import { google } from "googleapis";
-import { JWT } from "google-auth-library";
 import * as dotenv from "dotenv";
+import { JWT } from "google-auth-library";
 
 dotenv.config();
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
-const rawCredentials = process.env.SHEET_CREDENCIALES || "No";
-
-if (rawCredentials === "No") throw new Error("SHEET_CREDENCIALES no está definida en el entorno");
-
-const credentials = JSON.parse(rawCredentials);
-
-const auth = new google.auth.JWT({
-  email: credentials.client_email,
-  key: credentials.private_key,
-  scopes: SCOPES,
-});
-
-const sheets = google.sheets({ version: "v4", auth });
-
 export const getSheetData = async (spreadsheetId: string, range: string) => {
+  const rawCredentials = process.env.SHEET_CREDENCIALES;
+  if (!rawCredentials) throw new Error("Falta SHEET_CREDENCIALES");
+
+  const credentials = JSON.parse(rawCredentials);
+
+  const auth = new google.auth.JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    scopes: SCOPES,
+  });
+
+  const sheets = google.sheets({ version: "v4", auth });
+
   try {
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId,
       range,
     });
-
     return response.data.values || [];
   } catch (error) {
+    console.error("Error al acceder a Google Sheets:", error);
     return [];
   }
 };
