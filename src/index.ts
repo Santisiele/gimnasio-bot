84fadaa56828d514197405ad3b3463dfb3ecec75
Santiago Sielecki <santi.siele@gmail.com>
2025-07-08 23:09:03 +0000

    Sleep branch: bot desactivado

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-import express from "express";
-import { iniciarBot } from "@/bot";
-
-iniciarBot();
-
-const app = express();
-
-app.get("/", (_req, res) => {
-  res.send("Bot de Telegram corriendo correctamente en Railway.");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
-});
+console.log("Bot dormido");
